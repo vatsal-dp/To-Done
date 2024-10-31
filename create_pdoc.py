b102bd17b80ae6de11a809515d1ff5f7e14a5f1d
Charlie-Eastin <charles98310@gmail.com>
2024-10-31 02:33:55 -0400

    Update create_pdoc.py

--- create_pdoc.py
+++ create_pdoc.py
@@ -3,17 +3,12 @@ import pdoc
 
 OUTPUT_DIR = "./docs/todo"
 
-# Programmatically provide settings module
 SETTINGS_MODULE = "smarttodo.settings"
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', SETTINGS_MODULE)
 
-# Setup Django
 import django
-
 django.setup()
 
-# Ensure the output directory exists
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-# Generate HTML documentation and output to the specified directory
-pdoc.pdoc("todo", output_directory=OUTPUT_DIR, format="html")
+pdoc.pdoc("todo", output_directory=OUTPUT_DIR)
