8b5df263c148d1afbea6f8ed8a8c98da77e00b4a
Charlie-Eastin <charles98310@gmail.com>
2024-10-31 02:39:29 -0400

    Update create_pdoc.py

--- create_pdoc.py
+++ create_pdoc.py
@@ -5,14 +5,14 @@ from pathlib import Path
 
 OUTPUT_DIR = Path("./docs/todo")
 
+if OUTPUT_DIR.exists():
+    shutil.rmtree(OUTPUT_DIR)
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
 SETTINGS_MODULE = "smarttodo.settings"
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', SETTINGS_MODULE)
 
 import django
 django.setup()
-
-if OUTPUT_DIR.exists():
-    shutil.rmtree(OUTPUT_DIR)
-OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
 pdoc.pdoc("todo", output_directory=OUTPUT_DIR)
