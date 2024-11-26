6ae6395396242e514bf0272521f529f66115f1fb
smitraval24 <smit24raval@gmail.com>
2024-11-26 02:54:24 -0500

    added middleware

--- todo/settings.py
+++ todo/settings.py
@@ -7,8 +7,15 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'myapp',
     'crispy_forms',  # Ensure crispy_forms is listed here
-    'crispy_bootstrap4',  # Add this if you're using the Bootstrap 4 template pack
+    'crispy_bootstrap4',
+    'django.contrib.messages', 
 ]
+
+
+MIDDLEWARE = [
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'  # or 'bootstrap5' if you're using Bootstrap 5
 
